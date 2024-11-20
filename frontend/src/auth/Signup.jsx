14aabe32b1ce7614781e1143e152200d0cdf4365
Nikhil Kumar maurya <72245137+nikhil8483@users.noreply.github.com>
2024-11-21 00:37:19 +0530

    Update Signup.jsx

--- frontend/src/auth/Signup.jsx
+++ frontend/src/auth/Signup.jsx
@@ -30,7 +30,7 @@ const Signup = () => {
     }
 
     try {
-      const url_signup="https://crud-app-1-api.vercel.app/?vercelToolbarCode=QcHcmEBps1qOiNS/auth/signup";
+      const url_signup="https://crud-app-1-api.vercel.app/auth/signup";
       const response = await fetch(url_signup, {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
